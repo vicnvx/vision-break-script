50c8f928c4a7df206754c05b93de7a629aff7601
vicnvx <victormlira@outlook.com>
2022-05-02 14:47:05 -0500

    add visionbreak.ps1

---
+++ visionbreak.ps1
@@ -0,0 +1,10 @@
+﻿for(;;) {
+
+    [reflection.assembly]::loadwithpartialname('System.Windows.Forms')
+    [reflection.assembly]::loadwithpartialname('System.Drawing')
+    $notify = New-Object System.Windows.Forms.NotifyIcon
+    $notify.icon = [System.Drawing.SystemIcons]::Information
+    $notify.Visible = $true
+    $notify.ShowBalloonTip(10, 'Vision Break', 'take a look around...',[System.Windows.Forms.ToolTipIcon]::None)
+    Start-Sleep 1200
+}
